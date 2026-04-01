d3a9b73c54b1e9e4cb4727bbef54c5947ca7ab95
dumitrubolgari74-dot <dumitrubolgari74@gmail.com>
2026-04-01 12:32:25 +0300

    modification

---
+++ ShiftRun/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Start nu este necesar momentan, așa că îl putem lăsa gol sau șterge
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        // 1. Verificăm dacă suntem pe pământ
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // 2. Mișcare pe A și D (Axa Orizontală)
+        float x = Input.GetAxis("Horizontal");
+        Vector3 move = transform.right * x;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        // 3. Săritura pe SPACE
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // 4. Aplicăm gravitația (căderea)
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
